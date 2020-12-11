ea2182424e51e068c4902a765da0c2871ef7481c
Bryan Makendi <bryan.makendi@gmail.com>
2020-12-11 16:30:58 +0100

    suppression de toString et complétion de la méthode process pour AnalyticsCounter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,6 +1,8 @@
 package com.hemebiotech.analytics;
 
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
@@ -31,18 +33,18 @@ public class AnalyticsCounter {
 	 * méthode process qui call les méthodes getSymptoms etc
 	 */
 	
-	public void process() {
-		ReadSymptomDataFromFile reader = new ReadSymptomDataFromFile("symptoms.txt");
-		reader.getSymptoms();
-	}
-	
-	@Override
-	public String toString() {
+	public void process() throws IOException {
 		StringBuilder string = new StringBuilder();
+		ReadSymptomDataFromFile reader = new ReadSymptomDataFromFile("symptoms.txt");
+		FileWriter writer = new FileWriter("results.out");
+		for (String s : reader.getSymptoms()) {
+			this.increment(s);
+		}
 		for (Entry<String, Integer> symptom : symptoms.entrySet()) {
 			string.append(symptom.getKey()).append(", ").append(symptom.getValue()).append("\n");
+			writer.write(string.toString());
 		}
-		return string.toString();
+		writer.close();
 	}
 	
 	public static void main(String args[]) throws Exception {
