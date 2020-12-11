8e30de5b295aa12de969c0437962f92dda719842
Bryan Makendi <bryan.makendi@gmail.com>
2020-12-11 16:54:08 +0100

    Nouvelle méthode writes et méthode process terminée

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -6,6 +6,10 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
+/*TODO
+ * supprimer les doublons et trier les symptomes
+ */
+
 public class AnalyticsCounter {
 	private final HashMap<String, Integer> symptoms;
 	
@@ -29,17 +33,13 @@ public class AnalyticsCounter {
 		this.symptoms.put(s, value);
 	}
 	
-	/*TODO
-	 * méthode process qui call les méthodes getSymptoms etc
+	/**
+	 * Writes data from HashMap symptoms in a "result.out" file. 
+	 * @throws IOException
 	 */
-	
-	public void process() throws IOException {
+	public void writes() throws IOException {
 		StringBuilder string = new StringBuilder();
-		ReadSymptomDataFromFile reader = new ReadSymptomDataFromFile("symptoms.txt");
 		FileWriter writer = new FileWriter("results.out");
-		for (String s : reader.getSymptoms()) {
-			this.increment(s);
-		}
 		for (Entry<String, Integer> symptom : symptoms.entrySet()) {
 			string.append(symptom.getKey()).append(", ").append(symptom.getValue()).append("\n");
 			writer.write(string.toString());
@@ -47,7 +47,20 @@ public class AnalyticsCounter {
 		writer.close();
 	}
 	
+	/**
+	 * Reads symptoms from file "symptoms.txt" then writes them in a file using method AnalyticsCounter.writes().
+	 * @throws IOException
+	 */
+	public void process() throws IOException {
+		ReadSymptomDataFromFile reader = new ReadSymptomDataFromFile("symptoms.txt");
+		for (String s : reader.getSymptoms()) {
+			this.increment(s);
+		}
+		this.writes();
+	}
+	
 	public static void main(String args[]) throws Exception {
-		
+		AnalyticsCounter counter = new AnalyticsCounter();
+		counter.process();
 	}
 }
